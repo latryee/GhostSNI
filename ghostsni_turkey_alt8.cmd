97cdd4259350fc59782abf43a901a34392d25ea2
latryee <latryee@users.noreply.github.com>
2026-02-25 13:43:50 +0300

    v0.3.1: Absolute path ile exe arama - ZIP uyumlulugu

--- ghostsni_turkey_alt8.cmd
+++ ghostsni_turkey_alt8.cmd
@@ -9,38 +9,40 @@ if %errorlevel% neq 0 (
 
 cd /d "%~dp0"
 
-set "EXE="
-if exist "bin\GhostSNI.exe" set "EXE=bin\GhostSNI.exe"
-if exist "build\GhostSNI.exe" set "EXE=build\GhostSNI.exe"
-if exist "GhostSNI.exe" set "EXE=GhostSNI.exe"
-
-if "%EXE%"=="" (
-    echo.
-    echo  [HATA] GhostSNI.exe bulunamadi!
-    echo  Aranan: bin\ , build\ , .\ 
-    echo.
-    pause
-    exit /b 1
+if exist "%~dp0bin\GhostSNI.exe" (
+    start "" "%~dp0bin\GhostSNI.exe" -f 2 -e -b -r -s -m
+    goto :started
+)
+if exist "%~dp0build\GhostSNI.exe" (
+    start "" "%~dp0build\GhostSNI.exe" -f 2 -e -b -r -s -m
+    goto :started
+)
+if exist "%~dp0GhostSNI.exe" (
+    start "" "%~dp0GhostSNI.exe" -f 2 -e -b -r -s -m
+    goto :started
 )
 
+echo.
+echo  [HATA] GhostSNI.exe bulunamadi!
+echo  Script dizini: %~dp0
+echo  Aranan: %~dp0bin\GhostSNI.exe
+echo  Aranan: %~dp0build\GhostSNI.exe
+echo.
+pause
+exit /b 1
+
+:started
 echo.
 echo  GhostSNI baslatiliyor...
 echo  Profil: Frag + fake (passive DPI kapali)
 echo.
-
-start "" "%EXE%" -f 2 -e -b -r -s -m
-
 timeout /t 2 /nobreak >nul
-
 tasklist /fi "imagename eq GhostSNI.exe" 2>nul | find /i "GhostSNI.exe" >nul
 if %errorlevel%==0 (
     echo  [OK] GhostSNI arka planda calisiyor.
     echo  Kapatmak icin: Tray ikonu ^> Durdur ve Cik
-    echo.
     timeout /t 3
 ) else (
     echo  [HATA] GhostSNI baslatilamadi!
-    echo  Yonetici olarak calistirdiginizden emin olun.
-    echo.
     pause
 )
